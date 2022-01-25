3916919fc8b0f4f725a26ca255fbee5e9bbad810
aidan-starke <starke.aidan@gmail.com>
2022-01-26 11:29:10 +1300

    prettier format

--- types/exchange/index.ts
+++ types/exchange/index.ts
@@ -1,70 +1,69 @@
-import {ApiRx} from '@cennznet/api';
-import {Signer} from '@cennznet/api/types';
-import {InjectedWindowProvider} from '@polkadot/extension-inject/types';
-import {Observable} from 'rxjs/internal/Observable';
-import {Amount} from '../../utils/exchange/Amount';
-
+import { ApiRx } from "@cennznet/api";
+import { Signer } from "@cennznet/api/types";
+import { InjectedWindowProvider } from "@polkadot/extension-inject/types";
+import { Observable } from "rxjs/internal/Observable";
+import { Amount } from "../../utils/exchange/Amount";
 
 export interface FeeExchangeResult {
-    amount: Amount;
-    assetId: number;
+	amount: Amount;
+	assetId: number;
 }
 
 export interface IAccounts {
-    name: string;
-    assets?: Asset[];
-    address: string;
+	name: string;
+	assets?: Asset[];
+	address: string;
 }
 
 export interface IAssetSwap {
-    fromAsset: number;
-    toAsset: number;
-    fromAssetAmount: Amount;
-    toAssetAmount: Amount;
+	fromAsset: number;
+	toAsset: number;
+	fromAssetAmount: Amount;
+	toAssetAmount: Amount;
 }
 
 export interface IAppConfig {
-    ENDPOINT: string;
-    ENV: string;
-    ASSETS: Asset[];
-    FEE_BUFFER: number;
-    MAX_FEE_BUFFER: number;
-    MIN_FEE_BUFFER: number;
+	ENDPOINT: string;
+	ENV: string;
+	ASSETS: Asset[];
+	FEE_BUFFER: number;
+	MAX_FEE_BUFFER: number;
+	MIN_FEE_BUFFER: number;
 }
 
 export interface Asset {
-    symbol: string;
-    id: number;
-    decimalPlaces?: number;
+	symbol: string;
+	id: number;
+	decimalPlaces?: number;
 }
 
 export interface AmountParams {
-    amount: Amount;
-    assetId: number;
-    amountChange?: boolean;
+	amount: Amount;
+	assetId: number;
+	amountChange?: boolean;
 }
 
 export interface IOption {
-    label: string;
-    value: number | string;
+	label: string;
+	value: number | string;
 }
 
 declare global {
-    interface Window {
-        injectedWeb3: Record<string, InjectedWindowProvider>;
-        config: IAppConfig;
-        __REDUX_DEVTOOLS_EXTENSION__?: any;
-    }
+	interface Window {
+		injectedWeb3: Record<string, InjectedWindowProvider>;
+		config: IAppConfig;
+		__REDUX_DEVTOOLS_EXTENSION__?: any;
+	}
 }
 
 export interface IEpicDependency {
-    api$: Observable<ApiRx>;
+	api$: Observable<ApiRx>;
 }
 
 export interface IExtrinsic {
-    method: string;
-    params: any[];
-    price: Amount;
+	method: string;
+	params: any[];
+	price: Amount;
 }
 
 // export interface AssetOutputExtrinsic extends IExtrinsic {
@@ -73,120 +72,120 @@ export interface IExtrinsic {
 // }
 
 export interface IExchangePool {
-    coreAssetBalance: Amount;
-    assetBalance: Amount;
-    address: string;
-    assetId: number;
+	coreAssetBalance: Amount;
+	assetBalance: Amount;
+	address: string;
+	assetId: number;
 }
 
 export interface IUserShareInPool {
-    coreAssetBalance: Amount;
-    assetBalance: Amount;
-    liquidity: Amount;
-    address: string;
-    assetId: number;
+	coreAssetBalance: Amount;
+	assetBalance: Amount;
+	liquidity: Amount;
+	address: string;
+	assetId: number;
 }
 export interface IAssetBalance {
-    assetId: number;
-    account: string;
-    balance: Amount;
+	assetId: number;
+	account: string;
+	balance: Amount;
 }
 
 export interface IFee {
-    feeInCpay: Amount;
-    feeInFeeAsset: Amount;
+	feeInCpay: Amount;
+	feeInFeeAsset: Amount;
 }
 
 export interface CennznetInjected {
-    signer: Signer;
-    accounts$: Observable<IAccounts[]>;
-    accounts: IAccounts[];
+	signer: Signer;
+	accounts$: Observable<IAccounts[]>;
+	accounts: IAccounts[];
 }
 
 export interface SingleSourceInjected extends CennznetInjected {
-    isPaired$: Observable<boolean>;
-    isPaired: boolean;
-    pairedDevice$: Observable<{
-        version: string;
-        id: string;
-    }>;
-    pairedDevice;
+	isPaired$: Observable<boolean>;
+	isPaired: boolean;
+	pairedDevice$: Observable<{
+		version: string;
+		id: string;
+	}>;
+	pairedDevice;
 }
 
 export interface AddLiquidityFormData {
-    assetId: number;
-    coreAssetId: number;
-    assetAmount: Amount;
-    coreAmount: Amount;
-    feeAssetId: number;
-    investor: string;
-    buffer: number;
+	assetId: number;
+	coreAssetId: number;
+	assetAmount: Amount;
+	coreAmount: Amount;
+	feeAssetId: number;
+	investor: string;
+	buffer: number;
 }
 
 export interface IUserBalance {
-    assetBalance: Amount;
-    coreBalance: Amount;
-    investor: string;
+	assetBalance: Amount;
+	coreBalance: Amount;
+	investor: string;
 }
 
 export interface IAddLiquidity {
-    form: Partial<AddLiquidityFormData>;
-    investorBalance: IUserBalance;
+	form: Partial<AddLiquidityFormData>;
+	investorBalance: IUserBalance;
 }
 
 export interface RemoveLiquidityFormData {
-    asset: number;
-    liquidity: Amount;
-    feeAssetId: number;
-    investor: string;
-    buffer: number;
+	asset: number;
+	liquidity: Amount;
+	feeAssetId: number;
+	investor: string;
+	buffer: number;
 }
 
 export interface IRemoveLiquidity {
-    form: Partial<RemoveLiquidityFormData>;
-    estimatedAssetToWithdraw: {
-        core: Amount;
-        asset: Amount;
-    };
+	form: Partial<RemoveLiquidityFormData>;
+	estimatedAssetToWithdraw: {
+		core: Amount;
+		asset: Amount;
+	};
 }
 
 export interface ITxFeeParams {
-    extrinsicParams: any[];
-    feeAsset: number;
-    investor: string;
+	extrinsicParams: any[];
+	feeAsset: number;
+	investor: string;
 }
 
 export interface ExchangeFormData {
-    fromAsset: number;
-    toAsset: number;
-    fromAssetAmount: Amount;
-    toAssetAmount: Amount;
-    signingAccount: string;
-    extrinsic: string; //'buyAsset' | 'sellAsset'
-    feeAssetId: number;
-    buffer: number;
+	fromAsset: number;
+	toAsset: number;
+	fromAssetAmount: Amount;
+	toAssetAmount: Amount;
+	signingAccount: string;
+	extrinsic: string; //'buyAsset' | 'sellAsset'
+	feeAssetId: number;
+	buffer: number;
 }
 
 export interface SendFormData {
-    fromAsset: number;
-    toAsset: number;
-    fromAssetAmount: Amount;
-    toAssetAmount: Amount;
-    signingAccount: string;
-    recipientAddress: string;
-    extrinsic: string; //'buyAsset' | 'sellAsset'
-    feeAssetId: number;
-    buffer: number;
+	fromAsset: number;
+	toAsset: number;
+	fromAssetAmount: Amount;
+	toAssetAmount: Amount;
+	signingAccount: string;
+	recipientAddress: string;
+	extrinsic: string; //'buyAsset' | 'sellAsset'
+	feeAssetId: number;
+	buffer: number;
 }
 
 export interface LiquidityFormData {
-    assetId: number;
-    assetAmount: Amount;
-    coreAssetId: number;
-    coreAmount: Amount;
-    signingAccount: string;
-    extrinsic: string;
-    feeAssetId: number;
-    buffer: number;
-    type: string;
+	assetId: number;
+	assetAmount: Amount;
+	coreAssetId: number;
+	coreAmount: Amount;
+	signingAccount: string;
+	extrinsic: string;
+	feeAssetId: number;
+	buffer: number;
+	type: string;
 }
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -3,7 +3,7 @@ import GenericERC20TokenAbi from "../artifacts/GenericERC20Token.json";
 
 export const ETH = "0x0000000000000000000000000000000000000000";
 export const ETH_LOGO =
-  "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png";
+	"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png";
 
 export const getMetamaskBalance = async (ethereum, tokenAddress, account) => {
 	const provider = new ethers.providers.Web3Provider(ethereum);
