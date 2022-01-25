4ae3df0fad17132c8e8b90cb8148a9c414484ed6
aidan-starke <starke.aidan@gmail.com>
2022-01-25 14:32:13 +1300

    add ApiRx

--- providers/CENNZApiProvider.tsx
+++ providers/CENNZApiProvider.tsx
@@ -1,4 +1,3 @@
-import { Api } from "@cennznet/api";
 import {
   createContext,
   useState,
@@ -6,24 +5,34 @@ import {
   useContext,
   useCallback,
 } from "react";
+import { Api, ApiRx, } from "@cennznet/api";
+import { Observable } from "@cennznet/types";
 
 type CENNZApiContextType = {
   api: Api;
   updateApi: Function;
+  apiRx: Observable<ApiRx>;
+  updateApiRx: Function;
 };
 
 const CENNZApiContext = createContext<CENNZApiContextType>(null);
 
 export default function CENNZApiProvider({ children }: PropsWithChildren<{}>) {
   const [api, setApi] = useState<Api>(null);
+  const [apiRx, setApiRx] = useState<Observable<ApiRx>>(null);
 
   const updateApi = useCallback((endpoint) => {
     const instance = new Api({ provider: endpoint });
     instance.isReady.then(() => setApi(instance));
   }, []);
 
+  const updateApiRx = useCallback((endpoint) => {
+    const instance = ApiRx.create({ provider: endpoint })
+    setApiRx(instance)
+  }, [])
+
   return (
-    <CENNZApiContext.Provider value={{ api, updateApi }}>
+    <CENNZApiContext.Provider value={{ api, updateApi, apiRx, updateApiRx }}>
       {children}
     </CENNZApiContext.Provider>
   );
