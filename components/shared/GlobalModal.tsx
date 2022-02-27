2a6c1f51081b1e05416fa57d9e476f854bfa4a5f
Eug <eupston130@hotmail.com>
2022-02-27 15:00:21 -0700

    updated modal with css-js styling fixed centering

--- components/shared/GlobalModal.tsx
+++ components/shared/GlobalModal.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Backdrop, Modal } from "@mui/material";
-import { Heading, StyledModal } from "@/components/StyledComponents";
-import styles from "@/styles/components/shared/GlobalModal.module.scss";
+import { css } from "@emotion/react";
 
 const GlobalModal: React.FC<{}> = ({}) => {
 	const [open] = useState(true);
@@ -14,37 +13,82 @@ const GlobalModal: React.FC<{}> = ({}) => {
 			open={open}
 		>
 			<Modal open={open}>
-				<StyledModal
-					sx={{
-						position: "relative",
-						height: "202px",
-						width: "552px",
-						justifyContent: "center",
-						alignItems: "center",
-						display: "flex",
-						flexDirection: "column",
-						background: "#FFFFFF",
-						border: "transparent",
-						textAlign: "center",
-						top: "50%",
-						left: "30%",
-						boxShadow:
-							"0px 16px 24px rgba(0, 0, 0, 0.14), 0px 6px 30px rgba(0, 0, 0, 0.12), 0px 8px 10px rgba(0, 0, 0, 0.2);",
-					}}
-				>
-					<div className={styles.contentContainer}>
-						<h1 className={styles.header}>TITLE</h1>
-						<p className={styles.infoText}>
+				<div css={styles.styledModal}>
+					<div css={styles.contentContainer}>
+						<h1 css={styles.header}>TITLE</h1>
+						<p css={styles.infoText}>
 							Message here. Lorem ipsum dolor sit amet, consectetur adipiscing
 							elit, sed do eiusmod tempor incididunt ut labore et dolore magna
 							aliqua. Ut enim ad minim veniam
 						</p>
-						<button className={styles.confirmButton}>Confirm</button>
+						<button css={styles.confirmButton}>Confirm</button>
 					</div>
-				</StyledModal>
+				</div>
 			</Modal>
 		</Backdrop>
 	);
 };
 
 export default GlobalModal;
+
+export const styles = {
+	styledModal: css`
+		position: absolute;
+		height: auto;
+		width: 550px;
+		justify-content: center;
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		background: #ffffff;
+		border: transparent;
+		text-align: center;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+
+		box-shadow: 0px 16px 24px rgba(0, 0, 0, 0.14),
+			0px 6px 30px rgba(0, 0, 0, 0.12), 0px 8px 10px rgba(0, 0, 0, 0.2);
+	`,
+	contentContainer: css`
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		justify-content: flex-start;
+		width: 100%;
+		height: 100%;
+		padding: 18px 24px 18px 24px;
+	`,
+	header: css`
+		font-style: normal;
+		font-weight: bold;
+		font-size: 24px;
+		line-height: 24px;
+		letter-spacing: 0.15px;
+		color: #1130ff;
+	`,
+	infoText: css`
+		text-align: left;
+		margin: 0;
+	`,
+	confirmButton: css`
+		cursor: pointer;
+		height: 30px;
+		width: 120px;
+		font-family: Roboto;
+		font-style: normal;
+		font-weight: 500;
+		font-size: 14px;
+		line-height: 16px;
+		color: #1130ff;
+		letter-spacing: 1.25px;
+		text-transform: uppercase;
+		align-self: flex-end;
+		border: 1px solid #1130ff;
+		background: transparent;
+		&:hover {
+			background: #1130ff;
+			color: white;
+		}
+	`,
+};
