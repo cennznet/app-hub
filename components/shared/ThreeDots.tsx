bf736a6b3071a37f9b4008e82636f0dfa6b69303
Ken Vu <97480229+ken-centrality@users.noreply.github.com>
2022-02-28 10:01:39 +1300

    Remove `sass` package (#81)
    * Re-add `connectWallet` functionality to the Wallet button
    
    ... plus identify different wallet states to update UI accordingly
    
    * Move Identicon outside of React rendering so Next can do its optimisation
    
    * Avoid showing loading progress as `balances` should be all ready by when open
    
    * Update `tsconfig.json` to allow using `css` property on DOM element
    
    * Bring AccountIdenticon to the Wallet plus fix nesting error due `div` inside `p`
    
    * Pass modal open state straight down to avoid state out of sync
    
    * Opt for a simpler picker to select the account
    
    * Move styling into `styles` object for legibility
    
    * Update Wallet and WalletModal styling
    
    ... plus improving connect flow and switching account flow
    
    * Re-style the WalletModal panel
    
    ... also merge `AccountBalances` with `WalletModal`
    
    * Add check to `balances.length` in PoolForm
    
    * Pass props to `styles` object via wrapped functions
    
    * Switch over to use `css` prop for `PageBackdrop` component
    
    * Switch over to use `css` prop for `PageFrame` component
    
    * Switch over to use `css` prop for `ThreeDots` component
    
    * Remove `sass` package

--- components/shared/ThreeDots.tsx
+++ components/shared/ThreeDots.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
-import styles from "@/styles/components/shared/ThreeDots.module.scss";
+import { css } from "@emotion/react";
 
 const ThreeDots: FC<{}> = () => {
 	return (
-		<div className={styles.root}>
+		<div css={styles.root}>
 			<span>.</span>
 			<span>.</span>
 			<span>.</span>
@@ -12,3 +12,37 @@ const ThreeDots: FC<{}> = () => {
 };
 
 export default ThreeDots;
+
+export const styles = {
+	root: css`
+		@keyframes dot {
+			0% {
+				opacity: 0;
+			}
+			50% {
+				opacity: 1;
+			}
+			100% {
+				opacity: 0;
+			}
+		}
+
+		display: inline-block;
+		span {
+			opacity: 0;
+			animation: dot 1s infinite;
+
+			&:nth-child(1) {
+				animation-delay: 0s;
+			}
+
+			&:nth-child(2) {
+				animation-delay: 0.1s;
+			}
+
+			&:nth-child(3) {
+				animation-delay: 0.2s;
+			}
+		}
+	`,
+};
