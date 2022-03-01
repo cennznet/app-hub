3b8a5b2df939e239ac4a751df345aacedfbb81cc
Ken Vu <ken.vu@centrality.ai>
2022-03-01 17:15:30 +1300

    Remove un-used types

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,41 +4,6 @@ import { InjectedWindowProvider } from "@polkadot/extension-inject/types";
 import { Observable } from "rxjs/internal/Observable";
 import { Amount } from "@/utils/Amount";
 
-declare global {
-	interface Window {
-		injectedWeb3: Record<string, InjectedWindowProvider>;
-		config: IAppConfig;
-		__REDUX_DEVTOOLS_EXTENSION__?: any;
-	}
-}
-
-export interface FeeExchangeResult {
-	amount: Amount;
-	assetId: number;
-}
-
-export interface IAccounts {
-	name: string;
-	assets?: Asset[];
-	address: string;
-}
-
-export interface IAssetSwap {
-	fromAsset: number;
-	toAsset: number;
-	fromAssetAmount: Amount;
-	toAssetAmount: Amount;
-}
-
-export interface IAppConfig {
-	ENDPOINT: string;
-	ENV: string;
-	ASSETS: Asset[];
-	FEE_BUFFER: number;
-	MAX_FEE_BUFFER: number;
-	MIN_FEE_BUFFER: number;
-}
-
 export interface Asset {
 	symbol: string;
 	id?: number;
@@ -59,27 +24,6 @@ export interface BridgeToken {
 	id: number;
 }
 
-export interface AmountParams {
-	amount: Amount;
-	assetId: number;
-	amountChange?: boolean;
-}
-
-export interface IOption {
-	label: string;
-	value: number | string;
-}
-
-export interface IEpicDependency {
-	api$: Observable<ApiRx>;
-}
-
-export interface IExtrinsic {
-	method: string;
-	params: any[];
-	price: Amount;
-}
-
 export interface IExchangePool {
 	coreAssetBalance: Amount;
 	assetBalance: Amount;
@@ -93,110 +37,6 @@ export interface IUserShareInPool {
 	liquidity: Amount;
 	address: string;
 	assetId: number;
-}
-export interface IAssetBalance {
-	assetId: number;
-	account: string;
-	balance: Amount;
-}
-
-export interface IFee {
-	feeInCpay: Amount;
-	feeInFeeAsset: Amount;
-}
-
-export interface CennznetInjected {
-	signer: Signer;
-	accounts$: Observable<IAccounts[]>;
-	accounts: IAccounts[];
-}
-
-export interface SingleSourceInjected extends CennznetInjected {
-	isPaired$: Observable<boolean>;
-	isPaired: boolean;
-	pairedDevice$: Observable<{
-		version: string;
-		id: string;
-	}>;
-	pairedDevice;
-}
-
-export interface AddLiquidityFormData {
-	assetId: number;
-	coreAssetId: number;
-	assetAmount: Amount;
-	coreAmount: Amount;
-	feeAssetId: number;
-	investor: string;
-	buffer: number;
-}
-
-export interface IUserBalance {
-	assetBalance: Amount;
-	coreBalance: Amount;
-	investor: string;
-}
-
-export interface IAddLiquidity {
-	form: Partial<AddLiquidityFormData>;
-	investorBalance: IUserBalance;
-}
-
-export interface RemoveLiquidityFormData {
-	asset: number;
-	liquidity: Amount;
-	feeAssetId: number;
-	investor: string;
-	buffer: number;
-}
-
-export interface IRemoveLiquidity {
-	form: Partial<RemoveLiquidityFormData>;
-	estimatedAssetToWithdraw: {
-		core: Amount;
-		asset: Amount;
-	};
-}
-
-export interface ITxFeeParams {
-	extrinsicParams: any[];
-	feeAsset: number;
-	investor: string;
-}
-
-export interface ExchangeFormData {
-	fromAsset: number;
-	toAsset: number;
-	fromAssetAmount: Amount;
-	toAssetAmount: Amount;
-	signingAccount: string;
-	extrinsic: string; //'buyAsset' | 'sellAsset'
-	feeAssetId: number;
-	buffer: number;
-}
-
-export interface SendFormData {
-	fromAsset: number;
-	toAsset: number;
-	fromAssetAmount: Amount;
-	toAssetAmount: Amount;
-	signingAccount: string;
-	recipientAddress: string;
-	extrinsic: string; //'buyAsset' | 'sellAsset'
-	feeAssetId: number;
-	buffer: number;
-}
-
-export interface LiquidityFormData {
-	assetId: number;
-	assetAmount: Amount;
-	coreAssetId: number;
-	coreAmount: Amount;
-	signingAccount: string;
-	extrinsic: string;
-	feeAssetId: number;
-	buffer: number;
-	type: string;
 }
 
 export interface AssetInfo {
