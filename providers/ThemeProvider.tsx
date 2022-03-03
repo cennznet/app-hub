e9e732a4801af22a422be0176392059f5c08266f
Ken Vu <ken.vu@centrality.ai>
2022-03-03 20:24:33 +1300

    Config `ThemeProvider` to match with the current section color

--- providers/ThemeProvider.tsx
+++ providers/ThemeProvider.tsx
@@ -1,35 +1,43 @@
-import { FC } from "react";
+import { FC, useMemo } from "react";
 import {
 	ThemeProvider as MuiThemeProvider,
 	createTheme,
+	Theme,
 } from "@mui/material/styles";
+import useSectionUri from "@/hooks/useSectionUri";
 
-declare module "@mui/material/styles" {
-	interface PaletteColorOptions {
-		main: string;
-		swap?: string;
-		pool?: string;
-		bridge?: string;
+declare module "@mui/material/styles/createPalette" {
+	export interface TypeText {
+		highlight?: string;
 	}
 
-	interface PaletteColor {
-		main: string;
+	export interface SimplePaletteColorOptions {
 		swap?: string;
 		pool?: string;
 		bridge?: string;
+		default?: string;
+	}
+
+	export interface PaletteColor {
+		swap?: string;
+		pool?: string;
+		bridge?: string;
+		default?: string;
 	}
 }
 
-const theme = createTheme({
+const config = {
 	palette: {
 		primary: {
 			main: "#1130FF",
+			default: "#1130FF",
 			swap: "#1130FF",
 			pool: "#9847FF",
 			bridge: "#2DC8CB",
 		},
 		secondary: {
-			main: "#B3BDFF",
+			main: "rgba(17, 48, 255, 0.35)",
+			default: "rgba(17, 48, 255, 0.35)",
 			swap: "rgba(17, 48, 255, 0.35)",
 			pool: "rgba(152, 71, 255, 0.35)",
 			bridge: "rgba(45, 200, 203, 0.35)",
@@ -40,6 +48,7 @@ const theme = createTheme({
 		text: {
 			primary: "#020202",
 			secondary: "#979797",
+			highlight: "#1130FF",
 		},
 	},
 	typography: {
@@ -55,9 +64,31 @@ const theme = createTheme({
 			},
 		},
 	},
-});
+};
 
 const ThemeProvider: FC<{}> = (props) => {
+	const section = useSectionUri();
+	const theme = useMemo<Theme>(() => {
+		return createTheme({
+			...config,
+			palette: {
+				...config.palette,
+				primary: {
+					...config.palette.primary,
+					main:
+						config.palette.primary[section] || config.palette.primary.default,
+				},
+
+				secondary: {
+					...config.palette.secondary,
+					main:
+						config.palette.secondary[section] ||
+						config.palette.secondary.default,
+				},
+			},
+		});
+	}, [section]);
+
 	return <MuiThemeProvider {...props} theme={theme} />;
 };
 
