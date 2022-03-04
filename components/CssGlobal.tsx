348787cb6fba38723fb1f68270cb34b107972390
Ken Vu <ken.vu@centrality.ai>
2022-03-04 14:59:13 +1300

    Add a global style rule to reduce font thickness

--- components/CssGlobal.tsx
+++ components/CssGlobal.tsx
@@ -5,6 +5,12 @@ const CssGlobal: FC<{}> = () => {
 	return (
 		<Global
 			styles={css`
+				html {
+					scroll-behavior: smooth;
+					-webkit-font-smoothing: antialiased;
+					-moz-osx-font-smoothing: grayscale;
+				}
+
 				body {
 					font-size: 16px;
 					line-height: 1.25;
