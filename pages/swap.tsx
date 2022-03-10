eed30d30039385576c9e0b8799e620292f796aed
Ken Vu <ken.vu@centrality.ai>
2022-03-11 11:45:05 +1300

    Add `MainPanel` and use it to house content for all sections

--- pages/swap.tsx
+++ pages/swap.tsx
@@ -11,6 +11,7 @@ import SwapAssetsPair from "@/components/SwapAssetsPair";
 import SwapStats from "@/components/SwapStats";
 import SwapSettings from "@/components/SwapSettings";
 import SwapProgress from "@/components/SwapProgress";
+import MainPanel from "@/components/MainPanel";
 
 export async function getStaticProps() {
 	const api = await Api.create({ provider: API_URL });
@@ -28,45 +29,16 @@ const Swap: React.FC<{ supportedAssets: CENNZAsset[] }> = ({
 }) => {
 	return (
 		<SwapProvider supportedAssets={supportedAssets}>
-			<div css={styles.root}>
-				<h1 css={styles.heading}>SWAP</h1>
-
+			<MainPanel defaultTitle="Swap">
 				<SwapForm>
 					<SwapAssetsPair />
 					<SwapStats />
 					<SwapSettings />
 				</SwapForm>
 				<SwapProgress />
-			</div>
+			</MainPanel>
 		</SwapProvider>
 	);
 };
 
 export default Swap;
-
-const styles = {
-	root: ({ shadows }: Theme) => css`
-		display: flex;
-		flex-direction: column;
-		justify-content: flex-start;
-		align-items: center;
-		width: 550px;
-		border-radius: 4px;
-		margin: 0 auto 5em;
-		position: relative;
-		background-color: #ffffff;
-		box-shadow: ${shadows[1]};
-		padding: 1.5em 2.5em 2.5em;
-		overflow: hidden;
-	`,
-
-	heading: ({ palette }: Theme) => css`
-		font-weight: bold;
-		font-size: 20px;
-		line-height: 1;
-		text-align: center;
-		text-transform: uppercase;
-		color: ${palette.primary.main};
-		margin-bottom: 1.5em;
-	`,
-};
