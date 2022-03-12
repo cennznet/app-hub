e154e8eb7953a9200f5bb5279f755d2a3efec6f5
Ken Vu <ken.vu@centrality.ai>
2022-03-12 19:52:18 +1300

    Add `NEXT_PUBLIC_ALLOWED_ASSET_IDS` environment

--- constants.ts
+++ constants.ts
@@ -1,3 +1,13 @@
-export const CENNZ_ASSET_ID: number = Number(process.env.NEXT_PUBLIC_CENNZ_ID);
-export const CPAY_ASSET_ID: number = Number(process.env.NEXT_PUBLIC_CPAY_ID);
+export const CENNZ_ASSET_ID: number = Number(
+	process.env.NEXT_PUBLIC_CENNZ_ASSET_ID
+);
+export const CPAY_ASSET_ID: number = Number(
+	process.env.NEXT_PUBLIC_CPAY_ASSET_ID
+);
+export const ALLOWED_ASSET_IDS: number[] =
+	process.env.NEXT_PUBLIC_CPAY_ASSET_ID.split(",").map(Number);
 export const API_URL: string = process.env.NEXT_PUBLIC_API_URL;
+
+export const ETH_CHAIN_ID: number = Number(
+	process.env.NEXT_PUBLIC_ETH_CHAIN_ID
+);
