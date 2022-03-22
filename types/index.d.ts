8440989308b366fcf117ccacfb912685b5fab6df
Ken Vu <ken.vu@centrality.ai>
2022-03-22 16:13:15 +1300

    Remove commented types

--- types/index.d.ts
+++ types/index.d.ts
@@ -17,51 +17,6 @@ export interface Asset {
 	name?: string;
 }
 
-// export interface IExchangePool {
-// 	coreAssetBalance: Amount;
-// 	assetBalance: Amount;
-// 	address: string;
-// 	assetId: number;
-// }
-
-// export interface IUserShareInPool {
-// 	coreAssetBalance: Amount;
-// 	assetBalance: Amount;
-// 	liquidity: Amount;
-// 	address: string;
-// 	assetId: number;
-// }
-
-// export interface AssetInfo {
-// 	id: number;
-// 	symbol: string;
-// 	decimals: number;
-// 	logo: string;
-// }
-
-// export interface BalanceInfo {
-// 	id: number;
-// 	symbol: string;
-// 	decimals: number;
-// 	logo: string;
-// 	value: number;
-// 	rawValue: Codec;
-// 	tokenAddress?: string;
-// }
-
-// export interface PoolValues {
-// 	tradeAsset: number | string;
-// 	coreAsset: number | string;
-// 	tradeLiquidity?: number;
-// 	coreLiquidity?: number;
-// }
-
-// export interface PoolSummaryProps {
-// 	tradeAsset: CENNZAsset;
-// 	poolLiquidity: PoolValues;
-// 	exchangeRate: number | string;
-// }
-
 export interface PoolConfig {
 	tradeAsset: CENNZAsset;
 	coreAsset: CENNZAsset;
