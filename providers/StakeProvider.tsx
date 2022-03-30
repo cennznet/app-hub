87a6ce0c52493fa34979d886e8968a402b05c5f5
aidan-starke <starke.aidan@gmail.com>
2022-03-31 12:07:46 +1300

    fetch election info

--- providers/StakeProvider.tsx
+++ providers/StakeProvider.tsx
@@ -4,15 +4,19 @@ import {
 	FC,
 	SetStateAction,
 	useContext,
+	useEffect,
 	useState,
 } from "react";
-import { CENNZAsset, StakeAction } from "@/types";
+import { CENNZAsset, StakeAction, ElectionInfo } from "@/types";
+import { useCENNZApi } from "@/providers/CENNZApiProvider";
 
 interface StakeContextType {
 	stakeAsset: CENNZAsset;
 
 	stakeAction: StakeAction;
 	setStakeAction: Dispatch<SetStateAction<StakeAction>>;
+
+	electionInfo: ElectionInfo;
 }
 
 const StakeContext = createContext<StakeContextType>({} as StakeContextType);
@@ -22,14 +26,34 @@ interface StakeProviderProps {
 }
 
 const StakeProvider: FC<StakeProviderProps> = ({ children, stakeAsset }) => {
+	const { api } = useCENNZApi();
 	const [stakeAction, setStakeAction] = useState<StakeAction>();
+	const [electionInfo, setElectionInfo] = useState<ElectionInfo>();
+
+	useEffect(() => {
+		if (!api) return;
+
+		const fetchElectionInfo = async () => {
+			const [elected, waiting] = await Promise.all([
+				api.derive.stakingCennznet.electedInfo,
+				api.derive.stakingCennznet.waitingInfo,
+			]);
+
+			setElectionInfo({ elected: await elected(), waiting: await waiting() });
+		};
+
+		void fetchElectionInfo();
+	}, [api]);
 
 	return (
 		<StakeContext.Provider
 			value={{
 				stakeAsset,
+
 				stakeAction,
 				setStakeAction,
+
+				electionInfo,
 			}}
 		>
 			{children}
