70a072a554f59d634b25328fe826a30498a2c6e8
aidan-starke <starke.aidan@gmail.com>
2022-04-06 10:07:32 +1200

    display elected & waiting validators, add `commission` & `nominate` columns

--- components/StakeElected.tsx
+++ components/StakeElected.tsx
@@ -1,7 +1,8 @@
-import { useCallback, useEffect, useState, VFC } from "react";
+import { useCallback, useState, VFC } from "react";
 import { css } from "@emotion/react";
 import { useStake } from "@/providers/StakeProvider";
 import {
+	Checkbox,
 	LinearProgress,
 	Table,
 	TableBody,
@@ -13,66 +14,25 @@ import {
 	Tooltip,
 } from "@mui/material";
 import { Balance, getTokenLogo } from "@/utils";
-import { DeriveStakingQuery, ElectedCandidate, StakingElected } from "@/types";
+import { ElectedCandidate } from "@/types";
 import { ETH_CHAIN_ID } from "@/constants";
 import AccountIdenticon from "@/components/shared/AccountIdenticon";
 import { poolRegistry } from "@/utils/poolRegistry";
 import CheckCircleOutlinedIcon from "@mui/icons-material/CheckCircleOutlined";
 import PendingOutlinedIcon from "@mui/icons-material/PendingOutlined";
-import { hexToBn } from "@polkadot/util";
 
 const StakeElected: VFC = () => {
 	const { electionInfo, stakingAsset } = useStake();
 	const [openAccount, setOpenAccount] = useState<string>();
-	const [mounted, setMounted] = useState<boolean>(false);
-	const [elected, setElected] = useState<StakingElected>();
 
 	const chain = ETH_CHAIN_ID === 1 ? "CENNZnet Azalea" : "CENNZnet Nikau";
-
-	useEffect(() => {
-		if (!electionInfo) return setMounted(false);
-
-		const electedInfoMap = parseElectedInfo(
-			electionInfo.elected.info
-		) as ElectedCandidate[];
-
-		const nextElected = electionInfo.elected.nextElected.map((el) =>
-			el.toHuman()
-		);
-
-		const validators = electionInfo.elected.validators.map((el) =>
-			el.toHuman()
-		);
-
-		setElected({
-			electedInfoMap,
-			nextElected,
-			validators,
-		});
-
-		setMounted(true);
-	}, [electionInfo]);
-
-	const parseElectedInfo = (electedInfo: DeriveStakingQuery[]) => {
-		return electedInfo.map((info) => {
-			const electedInfo = {};
-			Object.keys(info).forEach((key) => {
-				try {
-					electedInfo[key] = info[key].toJSON();
-				} catch (_) {
-					electedInfo[key] = info[key];
-				}
-			});
-			return electedInfo;
-		});
-	};
 
 	const parseElectionBalance = useCallback(
 		(amount) => {
 			if (!stakingAsset) return;
 
 			const electionBalance = new Balance(
-				hexToBn(amount).toString(),
+				parseFloat(amount.replace(/,/g, "")),
 				stakingAsset
 			);
 
@@ -81,21 +41,113 @@ const StakeElected: VFC = () => {
 		[stakingAsset]
 	);
 
-	const isElected = (accountId: string): boolean =>
-		elected.nextElected.some((electedId) => electedId === accountId);
+	const _renderRows = (electedInfo: ElectedCandidate[], isElected: boolean) => {
+		return electedInfo.map((candidate) => (
+			<TableRow key={candidate.accountId} css={styles.candidate}>
+				{/* Account Identicon */}
+				<TableCell>
+					<Tooltip
+						css={styles.accountTooltip}
+						open={openAccount === candidate.accountId}
+						title={candidate.accountId}
+						placement="left"
+						arrow
+					>
+						<div
+							onMouseEnter={() => setOpenAccount(candidate.accountId)}
+							onMouseLeave={() => setOpenAccount(null)}
+						>
+							<AccountIdenticon
+								value={candidate.accountId}
+								theme="beachball"
+								size={40}
+								css={styles.identicon}
+							/>
+						</div>
+					</Tooltip>
+				</TableCell>
+				{/* Pool */}
+				<TableCell>
+					{poolRegistry[chain][candidate.accountId] ?? "Unknown"}
+				</TableCell>
+				{/* Commission */}
+				<TableCell sx={{ textAlign: "center" }}>
+					{candidate.validatorPrefs.commission}
+				</TableCell>
+				{/* Total Staked */}
+				<TableCell>
+					<p css={styles.number}>
+						{parseElectionBalance(
+							isElected
+								? candidate.exposure.total
+								: candidate.stakingLedger.active
+						)}
+					</p>
+				</TableCell>
+				{/* Status */}
+				<TableCell>
+					{isElected ? (
+						<CheckCircleOutlinedIcon css={styles.status(isElected)} />
+					) : (
+						<PendingOutlinedIcon css={styles.status(isElected)} />
+					)}
+				</TableCell>
+				{/* Nominate Checkbox */}
+				<TableCell>
+					<Checkbox
+						css={styles.nominate}
+						onClick={_validatorSelected}
+						value={candidate.accountId}
+					/>
+				</TableCell>
+			</TableRow>
+		));
+	};
+
+	const stashAddress = "5DVHuiWPrWomw1GxgXx6XuDCURPdDcv6YjLchobf156kwnZx";
+
+	const [accountIdVec, setAccountIdVec] = useState<string[]>([]);
+	const [isValid, setIsValid] = useState<boolean>(false);
+
+	const _validatorSelected = (element: any): void => {
+		const accountSelected: string = element.currentTarget.value;
+		const accounts: string[] = accountIdVec;
+
+		if (element.target.checked && accountSelected) {
+			accounts.push(accountSelected);
+		} else if (!element.target.checked && accounts.includes(accountSelected)) {
+			const index = accounts.indexOf(accountSelected);
+
+			if (index > -1) {
+				accounts.splice(index, 1);
+			}
+		}
+
+		setAccountIdVec(accounts);
+
+		if (accounts.length !== 0 && stashAddress !== null) {
+			setIsValid(true);
+			// setExtrinsic(api.tx.staking.nominate(accounts));
+		} else {
+			setIsValid(false);
+		}
+	};
 
 	return (
 		<div css={styles.root}>
+			<div css={styles.labelContainer}>
+				<label htmlFor="elected">elected</label>
+				{!electionInfo && <LinearProgress css={[styles.infoProgress]} />}
+			</div>
 			<TableContainer css={[styles.container]}>
-				{!mounted && <LinearProgress css={[styles.infoProgress]} />}
 				<Table>
 					<TableHead>
 						<TableRow>
 							<TableCell css={styles.tableHead}>Account</TableCell>
 							<TableCell css={styles.tableHead}>Pool</TableCell>
-							<TableCell css={styles.tableHead}>Status</TableCell>
+							<TableCell css={styles.tableHead}>Commission</TableCell>
 							<TableCell css={styles.tableHead} sx={{ textAlign: "center" }}>
-								<div css={styles.stakingAssetHead}>
+								<div css={styles.totalStakedHead}>
 									Total Staked
 									<img
 										src={getTokenLogo(stakingAsset.symbol).src}
@@ -103,50 +155,27 @@ const StakeElected: VFC = () => {
 									/>
 								</div>
 							</TableCell>
+							<TableCell css={styles.tableHead}>Status</TableCell>
+							<TableCell css={styles.tableHead}>Nominate</TableCell>
 						</TableRow>
 					</TableHead>
 					<TableBody>
-						{!!elected &&
-							elected.electedInfoMap.map((candidate) => (
-								<TableRow key={candidate.accountId} css={styles.candidate}>
-									<TableCell>
-										<Tooltip
-											css={styles.accountTooltip}
-											open={openAccount === candidate.accountId}
-											title={candidate.accountId}
-											placement="left"
-											arrow
-										>
-											<div
-												onMouseEnter={() => setOpenAccount(candidate.accountId)}
-												onMouseLeave={() => setOpenAccount(null)}
-											>
-												<AccountIdenticon
-													value={candidate.accountId}
-													theme="beachball"
-													size={40}
-													css={styles.identicon}
-												/>
-											</div>
-										</Tooltip>
-									</TableCell>
-									<TableCell>
-										{poolRegistry[chain][candidate.accountId] ?? "Unknown"}
-									</TableCell>
-									<TableCell>
-										{isElected(candidate.accountId) ? (
-											<CheckCircleOutlinedIcon css={styles.status(true)} />
-										) : (
-											<PendingOutlinedIcon css={styles.status(false)} />
-										)}
-									</TableCell>
-									<TableCell>
-										<p css={styles.number}>
-											{parseElectionBalance(candidate.stakingLedger.total)}
-										</p>
-									</TableCell>
-								</TableRow>
-							))}
+						{!!electionInfo?.elected &&
+							_renderRows(
+								electionInfo.elected.sort((a, b) =>
+									parseFloat(a.exposure.total) < parseFloat(b.exposure.total)
+										? 1
+										: -1
+								),
+								true
+							)}
+						{!!electionInfo?.waiting &&
+							_renderRows(
+								electionInfo.waiting.sort((a, b) =>
+									a.stakingLedger.active < b.stakingLedger.active ? 1 : -1
+								),
+								false
+							)}
 					</TableBody>
 				</Table>
 			</TableContainer>
@@ -168,10 +197,12 @@ const styles = {
 		}
 	`,
 
+	labelContainer: css`
+		display: inline-flex;
+	`,
+
 	infoProgress: css`
-		position: absolute;
-		top: 7.5em;
-		left: 27em;
+		margin: 0.4em 0.5em;
 		width: 25px;
 		border-radius: 10px;
 		opacity: 0.5;
@@ -190,9 +221,10 @@ const styles = {
 	tableHead: css`
 		max-height: 2em;
 		padding-bottom: -2em;
+		text-align: center;
 	`,
 
-	stakingAssetHead: css`
+	totalStakedHead: css`
 		display: inline-flex;
 
 		img {
@@ -215,7 +247,7 @@ const styles = {
 
 	identicon: css`
 		margin-top: 0.5em;
-		margin-right: 1em;
+		margin-left: 0.3em;
 	`,
 
 	status:
@@ -226,4 +258,9 @@ const styles = {
 				margin-left: 0.3em;
 				color: ${isElected ? palette.success.main : "#2DC8CB"};
 			`,
+
+	nominate: css`
+		cursor: pointer;
+		margin-left: 0.3em;
+	`,
 };
