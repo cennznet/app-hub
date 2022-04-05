9b78f8df83ff97a2f186b76bd818a07a9c3c9f5d
aidan-starke <starke.aidan@gmail.com>
2022-04-05 12:25:45 +1200

    fetch UNClaimed withdrawals

---
+++ utils/fetchUnclaimedWithdrawals.ts
@@ -0,0 +1,78 @@
+import { Api } from "@cennznet/api";
+import { BridgedEthereumToken, WithdrawClaim } from "@/types";
+import { ETH_CHAIN_ID } from "@/constants";
+import { Balance, fetchBridgeTokens } from "@/utils/index";
+
+/**
+ * Fetch unclaimed withdraws for the selected account
+ *
+ * @param {string} selectedAccount Address of the selected account
+ * @param {Api} api CENNZnet API
+ * @return {Promise<WithdrawClaim[]>} Array of unclaimed withdraws
+ */
+export default async function fetchUnclaimedWithdrawals(
+	selectedAccount: string,
+	api: Api
+): Promise<WithdrawClaim[]> {
+	let apiPrefix: string;
+
+	switch (ETH_CHAIN_ID) {
+		case 1:
+			apiPrefix = "https://bridge-contracts.centralityapp.com";
+			break;
+		case 42:
+			apiPrefix = "https://bridge-contracts.nikau.centrality.me";
+			break;
+	}
+
+	const unclaimedRaw = await fetch(
+		`${apiPrefix}/withdrawals/${selectedAccount}`
+	)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("No UNclaimed withdrawals found");
+			} else return response.json();
+		})
+		.catch((err) => console.log(err.message));
+
+	if (!unclaimedRaw) return;
+
+	return await Promise.all<WithdrawClaim[]>(
+		unclaimedRaw.withdrawals.map(async (withdrawal) => {
+			if (!!withdrawal.hasClaimed) return;
+
+			const bridgeTokens = await fetchBridgeTokens(api, "Withdraw");
+			const transferAsset = bridgeTokens.find(
+				(token: BridgedEthereumToken) =>
+					token.assetId === Number(withdrawal.assetId)
+			);
+			const transferAmount = new Balance(
+				withdrawal.amount.trim(),
+				transferAsset
+			);
+
+			return {
+				assetId: Number(withdrawal.assetId),
+				expiry: getExpiryString(withdrawal.expiresAt),
+				eventProofId: Number(withdrawal.proofId),
+				transferAsset: transferAsset as BridgedEthereumToken,
+				transferAmount,
+			};
+		})
+	);
+}
+
+const getExpiryString = (expiresAt: number): string => {
+	const expiry = expiresAt * 1000;
+	const distance = expiry - Date.now();
+
+	if (distance <= 0) return "Expired";
+
+	const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	const hours = Math.floor(
+		(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+	);
+	const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+	return `Expires in: ${days}d ${hours}h ${minutes}m`;
+};
