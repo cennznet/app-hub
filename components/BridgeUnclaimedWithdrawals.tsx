75016aa6540449691bd1e072798cc25c638f919d
aidan-starke <starke.aidan@gmail.com>
2022-04-19 12:58:45 +1200

    render unclaimed withdrawals in a table

---
+++ components/BridgeUnclaimedWithdrawals.tsx
@@ -0,0 +1,150 @@
+import { useLayoutEffect, useState, VFC } from "react";
+import { css } from "@emotion/react";
+import {
+	LinearProgress,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	Theme,
+} from "@mui/material";
+import { useHistoricalWithdrawRequest } from "@/hooks";
+import { WithdrawClaim } from "@/types";
+
+const getMinutesAndSeconds = (expiresAt) => {
+	const expiry = expiresAt * 1000;
+	const distance = expiry - Date.now();
+
+	if (distance <= 0) return "Expired";
+
+	const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	return `${minutes}m ${seconds}s`;
+};
+
+const _renderExpiry = (expiryRaw, expiryString) => {
+	const [expiry, setExpiry] = useState<string>("");
+	const [seconds, setSeconds] = useState<number>(0);
+
+	useLayoutEffect(() => {
+		if (expiryRaw * 1000 > Date.now() + 600000) return setExpiry(expiryString);
+
+		const intervalId = setInterval(() => {
+			setSeconds((seconds) => seconds + 1);
+			setExpiry(getMinutesAndSeconds(expiryRaw - seconds));
+		}, 1000);
+
+		return () => clearInterval(intervalId);
+	}, [expiryString, expiryRaw, seconds, setExpiry]);
+
+	return (
+		<TableCell css={[styles.column, styles.number]}>
+			{expiry && expiry}
+			{!expiry && <LinearProgress css={[styles.expiryProgress]} />}
+		</TableCell>
+	);
+};
+
+const BridgeUnclaimedWithdrawals: VFC<{
+	unclaimedWithdrawals: WithdrawClaim[];
+}> = ({ unclaimedWithdrawals }) => {
+	const processHistoricalRequest = useHistoricalWithdrawRequest();
+
+	return (
+		<TableContainer css={styles.container}>
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableCell css={styles.column}>ID</TableCell>
+						<TableCell css={styles.column}>Value</TableCell>
+						<TableCell css={styles.column}>Expiry</TableCell>
+						<TableCell css={styles.column}>Action</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{unclaimedWithdrawals.map((unclaimed) => (
+						<TableRow key={unclaimed.eventProofId}>
+							{/* ID */}
+							<TableCell css={[styles.column, styles.number]}>
+								{unclaimed.eventProofId}
+							</TableCell>
+							{/* Value */}
+							<TableCell css={[styles.column, styles.number]}>
+								{unclaimed.transferAmount.toInput()}{" "}
+								{unclaimed.transferAsset.symbol}
+							</TableCell>
+							{/* Expiry */}
+							{_renderExpiry(unclaimed.expiryRaw, unclaimed.expiry)}
+							{/* ID */}
+							<TableCell css={styles.column}>
+								{unclaimed.expiry !== "Expired" && (
+									<button
+										css={styles.claimButton}
+										onClick={() => processHistoricalRequest(unclaimed)}
+										type="button"
+									>
+										claim
+									</button>
+								)}
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
+};
+
+export default BridgeUnclaimedWithdrawals;
+
+const styles = {
+	container: css`
+		margin-top: 0.2em;
+		border: 1px solid rgba(0, 0, 0, 0.1);
+		border-radius: 4px;
+		overflow-y: auto;
+		white-space: nowrap;
+		max-height: 15em;
+	`,
+
+	column: css`
+		text-align: center;
+	`,
+
+	number: css`
+		font-family: "Roboto Mono", monospace;
+	`,
+
+	expiryProgress: css`
+		margin: 0 auto;
+		width: 25px;
+		border-radius: 10px;
+		opacity: 0.5;
+		transition: opacity 0.2s;
+	`,
+
+	claimButton: ({ palette, transitions }: Theme) => css`
+		border-radius: 4px;
+		height: 1.7em;
+		width: 4em;
+
+		background-color: white;
+		font-weight: bold;
+		cursor: pointer;
+		transition: background-color ${transitions.duration.short}ms,
+			color ${transitions.duration.short}ms;
+
+		font-size: 13px;
+		text-transform: uppercase;
+		border: 1px solid ${palette.primary.main};
+		color: ${palette.primary.main};
+
+		&:hover {
+			background-color: ${palette.primary.main};
+			color: white;
+		}
+	`,
+};
--- components/BridgeWithdrawAdvanced.tsx
+++ components/BridgeWithdrawAdvanced.tsx
@@ -5,13 +5,13 @@ import {
 	Accordion,
 	AccordionDetails,
 	AccordionSummary,
-	Divider,
 	LinearProgress,
 	Theme,
 } from "@mui/material";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import { useBridge } from "@/providers/BridgeProvider";
-import { useHistoricalWithdrawRequest, useUnclaimedWithdrawals } from "@/hooks";
+import { useUnclaimedWithdrawals } from "@/hooks";
+import BridgeUnclaimedWithdrawals from "@/components/BridgeUnclaimedWithdrawals";
 
 interface BridgeAdvancedProps {}
 
@@ -31,8 +31,6 @@ const BridgeWithdrawAdvanced: VFC<
 		if (expanded) void updateUnclaimedWithdrawals();
 	}, [updateUnclaimedWithdrawals, expanded]);
 
-	const processHistoricalRequest = useHistoricalWithdrawRequest();
-
 	return (
 		<div {...props} css={styles.root}>
 			<Accordion
@@ -47,44 +45,23 @@ const BridgeWithdrawAdvanced: VFC<
 					Advanced
 				</AccordionSummary>
 				<AccordionDetails>
-					<div css={styles.unclaimedWithdrawals}>
-						{!advancedMounted && (
-							<LinearProgress css={[styles.claimCheckProgress]} />
+					<br />
+					<label htmlFor="unclaimedWithdrawalsTable">
+						{(!advancedMounted || someUnclaimed) && (
+							<span>unclaimed withdrawals</span>
 						)}
 						{advancedMounted && !someUnclaimed && (
-							<p>No UNclaimed Withdrawals &nbsp;&#127881;</p>
+							<span>no unclaimed withdrawals &nbsp;&#127881;</span>
 						)}
-						{advancedMounted && someUnclaimed && (
-							<div>
-								<p>UNclaimed Withdrawals: </p>
-								{unclaimedWithdrawals.map((unclaimed, i) => (
-									<div key={i}>
-										<div css={styles.unclaimed}>
-											<span style={{ display: "block" }}>
-												<p>
-													UNclaimed: {unclaimed.transferAmount.toInput()}{" "}
-													{unclaimed.transferAsset.symbol}
-												</p>
-												<p>{unclaimed.expiry}</p>
-											</span>
-
-											{unclaimed.expiry !== "Expired" && (
-												<button
-													css={styles.claimButton}
-													onClick={() => processHistoricalRequest(unclaimed)}
-													type="button"
-												>
-													claim
-												</button>
-											)}
-										</div>
-
-										<Divider css={styles.unclaimedDivider} />
-									</div>
-								))}
-							</div>
-						)}
-					</div>
+					</label>
+					{!advancedMounted && (
+						<LinearProgress css={[styles.claimCheckProgress]} />
+					)}
+					{advancedMounted && someUnclaimed && (
+						<BridgeUnclaimedWithdrawals
+							unclaimedWithdrawals={unclaimedWithdrawals}
+						/>
+					)}
 				</AccordionDetails>
 			</Accordion>
 		</div>
@@ -96,6 +73,11 @@ export default BridgeWithdrawAdvanced;
 const styles = {
 	root: css`
 		margin-top: 1em;
+
+		label {
+			text-transform: uppercase;
+			font-size: 14px;
+		}
 	`,
 
 	accordion: css`
@@ -123,62 +105,13 @@ const styles = {
 		}
 	`,
 
-	unclaimedWithdrawals: css`
-		margin-top: 1em;
-		margin-bottom: 1em;
-		display: block;
-
-		p {
-			font-weight: bold;
-			font-size: 14px;
-			display: block;
-		}
-	`,
-
 	claimCheckProgress: css`
+		display: inline-flex;
+		margin-left: 1em;
+		margin-bottom: 0.2em;
 		width: 25px;
 		border-radius: 10px;
 		opacity: 0.5;
 		transition: opacity 0.2s;
-	`,
-
-	unclaimed: css`
-		margin-bottom: 0.5em;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-
-		p {
-			font-family: "Roboto Mono", monospace;
-			margin-bottom: 1em;
-		}
-	`,
-
-	claimButton: ({ palette, transitions }: Theme) => css`
-		display: inline-block;
-		border-radius: 4px;
-		height: 2em;
-		width: 5em;
-		//align-self: center;
-
-		background-color: white;
-		font-weight: bold;
-		cursor: pointer;
-		transition: background-color ${transitions.duration.short}ms,
-			color ${transitions.duration.short}ms;
-
-		font-size: 14px;
-		text-transform: uppercase;
-		border: 1px solid ${palette.primary.main};
-		color: ${palette.primary.main};
-
-		&:hover {
-			background-color: ${palette.primary.main};
-			color: white;
-		}
-	`,
-
-	unclaimedDivider: css`
-		margin-bottom: 1em;
 	`,
 };
