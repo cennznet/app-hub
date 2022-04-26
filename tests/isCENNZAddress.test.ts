d50b1366c03116b4301ff47a45d7fc2377643cf2
aidan-starke <starke.aidan@gmail.com>
2022-04-27 08:30:45 +1200

    test `is[Ethereum/CENNZ]Address` utils

---
+++ tests/isCENNZAddress.test.ts
@@ -0,0 +1,17 @@
+import isCENNZAddress from "@/utils/isCENNZAddress"
+
+const CENNZAccount = global.getCENNZTestingAccount();
+const ethereumAccount = global.getEthereumTestingAccount();
+
+describe("isCENNZAddress", () => {
+  it("returns true if CENNZ address", () => {
+    const isCENNZ = isCENNZAddress(CENNZAccount)
+
+    expect(isCENNZ).toBe(true)
+  })
+  it("returns false if not CENNZ address", () => {
+    const isCENNZ = isCENNZAddress(ethereumAccount)
+
+    expect(isCENNZ).toBe(false)
+  })
+})
