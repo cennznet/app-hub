78c24e8b2f417fd4fe33316a51ab3b1ea353169c
aidan-starke <starke.aidan@gmail.com>
2022-04-28 12:08:08 +1200

    update styling

--- components/WalletSelect.tsx
+++ components/WalletSelect.tsx
@@ -2,6 +2,8 @@ import { VFC } from "react";
 import { css } from "@emotion/react";
 import { Theme } from "@mui/material";
 import { useWalletSelect } from "@/providers/WalletSelectProvider";
+import CENNZIconSVG from "@/assets/vectors/cennznet-icon.svg";
+import MetaMaskIconSVG from "@/assets/vectors/metamask.svg";
 
 const WalletSelect: VFC = () => {
 	const { setSelectedWallet } = useWalletSelect();
@@ -9,12 +11,33 @@ const WalletSelect: VFC = () => {
 	return (
 		<div css={styles.modalContent}>
 			<label htmlFor="select wallet">select wallet</label>
-			<div>
-				<div css={styles.walletActions}>
-					<span onClick={() => setSelectedWallet("CENNZnet")}>CENNZnet</span>
+			<div css={styles.selectOptions}>
+				<div
+					css={[styles.cennzButton, styles.selectOption]}
+					onClick={() => setSelectedWallet("CENNZnet")}
+				>
+					<div css={styles.walletIcon}>
+						<img
+							src={CENNZIconSVG.src}
+							alt="CENNZnet Logo"
+							css={styles.selectOptionImg}
+						/>
+					</div>
+					<span>CONNECT CENNZnet</span>
 				</div>
-				<div css={styles.walletActions}>
-					<span onClick={() => setSelectedWallet("MetaMask")}>MetaMask</span>
+				<br />
+				<div
+					css={[styles.metaMaskButton, styles.selectOption]}
+					onClick={() => setSelectedWallet("MetaMask")}
+				>
+					<div css={styles.walletIcon}>
+						<img
+							src={MetaMaskIconSVG.src}
+							alt="CENNZnet Logo"
+							css={styles.selectOptionImg}
+						/>
+					</div>
+					<span>CONNECT METAMASK</span>
 				</div>
 			</div>
 		</div>
@@ -24,7 +47,7 @@ const WalletSelect: VFC = () => {
 export default WalletSelect;
 
 const styles = {
-	modalContent: css`
+	modalContent: ({ palette }: Theme) => css`
 		position: absolute;
 		top: calc(4em + 48px);
 		right: 3em;
@@ -33,19 +56,77 @@ const styles = {
 		box-shadow: 4px 8px 8px rgba(17, 48, 255, 0.1);
 		border-radius: 4px;
 		outline: none;
+
+		label {
+			font-weight: bold;
+			font-size: 14px;
+			text-transform: uppercase;
+			margin: 1em 1em 0.5em;
+			display: block;
+			color: ${palette.primary.main};
+		}
 	`,
 
-	walletActions: ({ palette }: Theme) => css`
-		padding: 1em 1.5em 1.5em;
-		color: ${palette.grey["700"]};
+	selectOptions: css`
+		text-align: center;
+		display: block;
+		margin-top: 1em;
+	`,
 
-		> span {
-			transition: color 0.2s;
-			cursor: pointer;
+	selectOption: ({ transitions }: Theme) => css`
+		margin-bottom: 1em;
+		display: inline-flex;
+		padding: 0.75em;
+		border-radius: 4px;
+		height: 50px;
 
-			&:hover {
-				color: ${palette.primary.default};
-			}
+		background-color: white;
+		min-width: 240px;
+		font-weight: bold;
+		cursor: pointer;
+		transition: background-color ${transitions.duration.short}ms,
+			color ${transitions.duration.short}ms;
+	`,
+
+	cennzButton: ({ palette }: Theme) => css`
+		align-items: center;
+		justify-content: center;
+		border: 1px solid ${palette.primary.default};
+		color: ${palette.primary.default};
+		cursor: pointer;
+
+		&:hover {
+			background-color: ${palette.primary.default};
+			color: white;
 		}
+	`,
+
+	metaMaskButton: css`
+		align-items: center;
+		justify-content: center;
+		border: 1px solid #e2761b;
+		color: #e2761b;
+		cursor: pointer;
+
+		&:hover {
+			background-color: #e2761b;
+			color: white;
+		}
+	`,
+
+	selectOptionImg: css`
+		display: block;
+		width: 28px;
+		height: 28px;
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+	`,
+
+	walletIcon: css`
+		width: 28px;
+		height: 28px;
+		margin-right: 0.5em;
+		position: relative;
 	`,
 };
