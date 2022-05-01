5f378474c5b39fe9a0971829b5b079c65780c14c
Ken Vu <97480229+ken-centrality@users.noreply.github.com>
2022-05-02 10:37:15 +1200

    Add version number and commit hash to bottom of the page (#182)
    * Add version number and commit hash to bottom of the page
    
    * Use the correct env var for commit sha
    
    * Avoid adding the whole `package.json` to runtime JS

--- constants.ts
+++ constants.ts
@@ -39,3 +39,7 @@ export const CENNZ_EXPLORER_URL: string =
 
 export const ETH_EXPLORER_URL: string =
 	process.env.NEXT_PUBLIC_ETH_EXPLORER_URL;
+
+export const APP_VERSION: string = process.env.APP_VERSION;
+
+export const COMMIT_SHA: string = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA;
