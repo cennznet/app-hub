10dc01ac0e08107a74571a43c5f643810865aaca
aidan-starke <starke.aidan@gmail.com>
2022-05-05 12:07:31 +1200

    use `cvmToAddress` from `@cennznet/types/utils`

--- hooks/useSelectedAccount.ts
+++ hooks/useSelectedAccount.ts
@@ -1,9 +1,9 @@
+import { useMemo } from "react";
+import { cvmToAddress } from "@cennznet/types/utils";
 import { useCENNZWallet } from "@/providers/CENNZWalletProvider";
 import { useMetaMaskWallet } from "@/providers/MetaMaskWalletProvider";
 import { useWalletProvider } from "@/providers/WalletProvider";
-import { cvmToCENNZAddress } from "@/utils";
 import { InjectedAccountWithMeta } from "@polkadot/extension-inject/types";
-import { useMemo } from "react";
 
 export default function useSelectedAccount(): Partial<InjectedAccountWithMeta> {
 	const { selectedAccount: CENNZAccount } = useCENNZWallet();
@@ -13,6 +13,6 @@ export default function useSelectedAccount(): Partial<InjectedAccountWithMeta> {
 	return useMemo(() => {
 		if (selectedWallet === "CENNZnet") return CENNZAccount;
 		if (selectedWallet === "MetaMask" && !!metaMaskAccount?.address)
-			return { address: cvmToCENNZAddress(metaMaskAccount.address) };
+			return { address: cvmToAddress(metaMaskAccount.address) };
 	}, [CENNZAccount, metaMaskAccount, selectedWallet]);
 }
