4a9a35ec2d6dc3ceed13749bf9438a4f822f3d58
aidan-starke <starke.aidan@gmail.com>
2022-05-09 16:14:32 +1200

    disable bridge button & warn user if not connected to Ethereum

--- components/BridgeForm.tsx
+++ components/BridgeForm.tsx
@@ -7,6 +7,8 @@ import { useBridge } from "@/providers/BridgeProvider";
 import useBridgeStatus from "@/hooks/useBridgeStatus";
 import BridgeWithdrawAdvanced from "@/components/BridgeWithdrawAdvanced";
 import { useDepositRequest, useWithdrawRequest } from "@/hooks";
+import { useWalletProvider } from "@/providers/WalletProvider";
+import { ETH_CHAIN_ID } from "@/constants";
 
 interface BridgeFormProps {}
 
@@ -15,6 +17,8 @@ const BridgeForm: FC<IntrinsicElements["form"] & BridgeFormProps> = ({
 	...props
 }) => {
 	const { bridgeAction } = useBridge();
+	const { connectedChain } = useWalletProvider();
+
 	const [buttonLabel, setButtonLabel] = useState<string>("Deposit");
 
 	const processDepositRequest = useDepositRequest();
@@ -46,7 +50,7 @@ const BridgeForm: FC<IntrinsicElements["form"] & BridgeFormProps> = ({
 			<div css={styles.formSubmit}>
 				<SubmitButton
 					forceRequireMetaMask={true}
-					disabled={status === "Inactive"}
+					disabled={status === "Inactive" || connectedChain !== "Ethereum"}
 				>
 					{buttonLabel}
 				</SubmitButton>
@@ -54,6 +58,14 @@ const BridgeForm: FC<IntrinsicElements["form"] & BridgeFormProps> = ({
 				{status === "Inactive" && (
 					<div css={styles.formNote}>
 						{bridgeAction} is temporarily deactivated, please try again later.
+					</div>
+				)}
+
+				{!!connectedChain && connectedChain !== "Ethereum" && (
+					<div css={styles.formNote}>
+						Please connect to{" "}
+						{ETH_CHAIN_ID === 1 ? "Ethereum Mainnet" : "Kovan Test Network"} in
+						MetaMask.
 					</div>
 				)}
 			</div>
