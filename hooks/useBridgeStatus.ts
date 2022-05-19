efc37556fc5230467c6d67ff72f29bd2aac34503
aidan-starke <starke.aidan@gmail.com>
2022-05-20 11:47:19 +1200

    Revert "only fetch bridge status if connected to Ethereum"
    This reverts commit b57cedb8ab5f57ef3070da2bc7d561cc530ebbf9.

--- hooks/useBridgeStatus.ts
+++ hooks/useBridgeStatus.ts
@@ -4,24 +4,22 @@ import { useMetaMaskWallet } from "@/providers/MetaMaskWalletProvider";
 import { BridgeStatus } from "@/types";
 import { fetchBridgeDepositStatus, fetchBridgeWithdrawStatus } from "@/utils";
 import { useEffect, useState } from "react";
-import { useWalletProvider } from "@/providers/WalletProvider";
 
 export default function useBridgeStatus(): BridgeStatus {
 	const [bridgeStatus, setBridgeStatus] = useState<BridgeStatus>("Active");
 	const { api } = useCENNZApi();
 	const { wallet } = useMetaMaskWallet();
 	const { bridgeAction } = useBridge();
-	const { connectedChain } = useWalletProvider();
 
 	useEffect(() => {
-		if (!api || !wallet || connectedChain !== "Ethereum") return;
+		if (!api || !wallet) return;
 
 		if (bridgeAction === "Deposit")
 			fetchBridgeDepositStatus(api, wallet).then(setBridgeStatus);
 
 		if (bridgeAction === "Withdraw")
 			fetchBridgeWithdrawStatus(api, wallet).then(setBridgeStatus);
-	}, [bridgeAction, api, wallet, connectedChain]);
+	}, [bridgeAction, api, wallet]);
 
 	return bridgeStatus;
 }
