70921eba9d631b148e3f9d00d15a7b62770ca52c
Eug <eupston130@hotmail.com>
2022-05-24 16:35:18 +1200

    fixed batch transfer decimal amount

--- utils/getBatchTransferAssetExtrinsic.ts
+++ utils/getBatchTransferAssetExtrinsic.ts
@@ -11,10 +11,12 @@ export default function getBatchTransferAssetExtrinsic(
 	recipient: string
 ): CENNZnetExtrinsic | SubmittableExtrinsic<"promise"> {
 	const transferTransactions = transferAssets.map((asset) => {
+		const transferValue =
+			parseFloat(asset.value.toBalance()) * 10 ** asset.decimals;
 		return api.tx.genericAsset.transfer(
 			asset.assetId,
 			recipient,
-			asset.value.toFixed(0)
+			transferValue.toString()
 		);
 	});
 	return api.tx.utility.batch(transferTransactions);
